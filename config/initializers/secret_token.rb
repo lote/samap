8bdbc470f81032e03b3fe72b8f5c97f4d86877b5
lote <your.email@example.com>
2013-09-08 11:43:16 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Triforce::Application.config.secret_token = '50c791b1c4d55e8c1432d78efb98887fb6f1d1de16d5bb9048d88d3574afa678a535e4b2af4a0636603b8b328d3f320108e5bf22a98e8ef03b0e35f771a7e2f4'
