c0daa8eb0e5a765a949bf120517cc108f7700b36
lote <your.email@example.com>
2013-09-08 11:57:21 +0300

    Finish ispravlenie

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Triforce::Application.config.secret_token = '50c791b1c4d55e8c1432d78efb98887fb6f1d1de16d5bb9048d88d3574afa678a535e4b2af4a0636603b8b328d3f320108e5bf22a98e8ef03b0e35f771a7e2f4'
+SampleApp::Application.config.secret_token = '0e080b69c1b0c993d5c5fe1dc6b84b77ca2277694eba50c78399551abbe2312def76f16cf8ba0172b2b5f3a8c0cafcc4ed9e7eb71a28979d1ee511add10c804b'
